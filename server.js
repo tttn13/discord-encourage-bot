50bddccae4eb9d768ff7ef9984952cb0c3713868
Thi Nguyen <tttn.tn13@gmail.com>
2021-08-06 19:14:55 -0700

    remove api from path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", require("./routes/botRoutes"));
+app.use("/", require("./routes/botRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
